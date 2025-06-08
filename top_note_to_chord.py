495335c31d978bf6bb7ac00ca08515ff73d4d56f
chaihahaha <chai836275709@gmail.com>
2025-06-09 00:42:58 +0800

    refactor code, add triad chords

--- top_note_to_chord.py
+++ top_note_to_chord.py
@@ -33,42 +33,56 @@ config = {
     #"strings": {"color": "rgb(200,200,200)", "width": 2},
 }
 
-chord_tension_structures = {
+# base chords
+base_chords = {
+"major":[0,4,7],
+"minor":[0,3,7],
+"dim":[0,3,6],
+"aug":[0,4,8],
 "major7":[0,4,7,11],
 "minor7":[0,3,7,10],
 "minor7flat5":[0,3,6,10],
 "minormajor7":[0,3,7,11],
 "major7aug":[0,4,8,11],
-"dim":[0,3,6,9],
+"dim7":[0,3,6,9],
 "dom7":[0,4,7,10],
 "dom7sus":[0,5,7,10],
 "dom7aug":[0,4,8,10],
-
-"major7_tensions":[2,6,9],
-"minor7_tensions":[2,5,9],
-"minor7flat5_tensions":[2,5,8],
-"minormajor7_tensions":[2,5,9],
-"major7aug_tensions":[2,6],
-"dim_tensions":[2,5,8,11],
-"dom7_tensions":[1,2,3,6,8,9],
-"dom7sus_tensions":[1,2,3,4,8,9],
-"dom7aug_tensions":[1,2,3,6,9],
 }
 
-ALL = fr.constants.CHROMATICS_NOTES
+tension_notes = {
+"major":[1,2,3,5,6,8,9,10,11],
+"minor":[2,5,6,8,9,10],
+"dim":[2,5,8,9,11],
+"aug":[1,2,3,6,9,10,11],
+"major7":[2,6,9],
+"minor7":[2,5,9],
+"minor7flat5":[2,5,8],
+"minormajor7":[2,5,9],
+"major7aug":[2,6],
+"dim7":[2,5,8,11],
+"dom7":[1,2,3,6,8,9],
+"dom7sus":[1,2,3,4,8,9],
+"dom7aug":[1,2,3,6,9],
+}
+
+# top note pos relative to the base chord root
+# key is base chord, value is the set of top notes
+top_notes = {}
+for k in base_chords.keys():
+    top_notes[k] = sorted(list(set(base_chords[k] + tension_notes[k])))
+
+NOTE_NAME = fr.constants.CHROMATICS_NOTES
 
 
-for k,v in chord_tension_structures.items():
-    for i, interval in enumerate(v):
+for chord_name, chord_notes_relroot in base_chords.items():
+    for i, top_note_pos in enumerate(top_notes[chord_name]):
         fretboard_config = FretBoardConfig.from_dict(config)
         fretboard = FretBoard(config=fretboard_config)
-        top = ALL[0]
-        root = ALL[(-interval) % 12]
-        if 'tensions' in k:
-            chord_name = k.removesuffix('_tensions')
-        else:
-            chord_name = k
-        chord = [ALL[(i-interval) % 12] for i in chord_tension_structures[chord_name]]
+        top = NOTE_NAME[0]
+        root = NOTE_NAME[(-top_note_pos) % 12]
+        chord = [NOTE_NAME[(chord_note-top_note_pos) % 12] for chord_note in base_chords[chord_name]]
+        print(f'{chord_name} chord: ', chord, base_chords[chord_name])
         container = fr.notes_creators.NotesContainer(root,chord)
         fingerings = container.get_chord_fingerings(TUNING)
         cand_fgs_loss = dict()
@@ -81,24 +95,14 @@ for k,v in chord_tension_structures.items():
                 difficulty, _ = scorer.analyze()
                 if difficulty < 9999:
                     cand_fgs_loss[tuple(fg_with_top)] = difficulty
-
-            #number_none = sum([n is None for n in fg_with_top])
-            #if number_none == 0:
-            #    numbers = np.array([n for n in fg_with_top if n is not None])
-            #    if np.all(numbers>=3) and np.all(numbers<=9):
-            #        min_n = np.min(numbers)
-            #        max_n = np.max(numbers)
-            #        range_pos = max_n - min_n
-            #        number_collinear = np.sum(numbers == min_n)
-            #        loss = 4*range_pos - number_collinear
-            #        cand_fgs_loss[tuple(fg_with_top)] = loss
         if not cand_fgs_loss:
-            print(f'{k} search failed!')
+            print(f'{chord_name} top note {top_note_pos} search failed!')
         else:
             best_fingering = min(cand_fgs_loss, key=lambda x:cand_fgs_loss[x])
-            print(k, best_fingering)
+            print(chord_name, best_fingering)
 
             fretboard.add_note(0, top)
             #fretboard.add_notes(scale=container)
             fretboard.add_fingering(list(best_fingering), root=root)
-            fretboard.export(f"svgs/{k}_{i}.png", format="png")
+            interval = fr.constants.CHROMATICS_INTERVALS[top_note_pos]
+            fretboard.export(f"svgs/{chord_name}_{interval}.png", format="png")
